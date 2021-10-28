01cfa51500984b198b1e754a2a686580733357e3
trevorspecht <trevor.specht@gmail.com>
2021-10-28 11:47:53 -0400

    1.9: unicafe, step 4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -7,19 +7,28 @@ const Button = ({ handleClick, text }) => (
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad
   const average = all / 3
-  const positive = (good / all) * 100
+  let positive = ((good / all) ? (good / all) * 100 : 0)
 
-  return (
-    <div>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p> 
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
-    </div>
-  )
+  if (all === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p> 
+        <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {average}</p>
+        <p>positive {positive} %</p>
+      </div>
+    )
+  }
 }
 
 const App = () => {
