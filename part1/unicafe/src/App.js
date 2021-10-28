348134886be2605e512d8571519acf32ed72fae0
trevorspecht <trevor.specht@gmail.com>
2021-10-28 00:16:50 -0400

    1.7: unicafe, step 2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -14,6 +14,10 @@ const App = () => {
   const neutralCounter = () => setNeutral(neutral + 1)
   const badCounter = () => setBad(bad + 1)
 
+  const all = good + neutral + bad
+  const average = all / 3
+  const positive = (good / all) * 100
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -24,6 +28,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p> 
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
