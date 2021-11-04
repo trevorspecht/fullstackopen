cc44f05110d19dd264bbadcfbc6f8f7a19e53a91
trevorspecht <trevor.specht@gmail.com>
2021-11-04 14:05:01 -0400

    2.10: phonebook, step 5

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,29 +1,22 @@
 import React, { useState } from 'react'
 
-const Person = ({ name, number }) => {
+const Person = (props) => {
   return (
-    <p>{name} {number}</p>
+    <p>{props.name} {props.number}</p>
   )
 }
 
-const App = () => {
-  const [ persons, setPersons ] = useState([]) 
-  const [newName, setNewName ] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [search, setSearch] = useState('')
+const PersonList = ({ persons }) => {
+  return (
+    <>
+      {persons.map(person => 
+        <Person key={person.key} name={person.name} number={person.number}/>
+      )}
+    </>
+  )
+}
 
-  let personsList = persons.map((person, index) => {
-    let keyObj = {key: index, name: person.name, number: person.number}
-    return keyObj
-  })
-
-  const handleSearch = (event) => {
-    const value = event.target.value
-    setSearch(value)
-    personsList = persons.filter(person => 
-      person.name.toLowerCase().indexOf(value.toLowerCase()) !== -1)
-    setPersons(personsList)
-  }
+const PersonForm = ({ persons, setPersons, newName, setNewName, newNumber, setNewNumber }) => {
 
   const handleNameChange = (event) => {
     const value = event.target.value
@@ -42,7 +35,7 @@ const App = () => {
   const addPerson = (event) => {
     event.preventDefault()
     const personObj = {
-      key: personsList.length,
+      key: persons.length,
       name: newName,
       number: newNumber
     }
@@ -53,23 +46,56 @@ const App = () => {
   }
 
   return (
+    <form onSubmit={addPerson}>
+    <div>
+      <p>name: <input value={newName} onChange={handleNameChange} /></p>
+      <p>number: <input value={newNumber} onChange={handleNumberChange} /></p>
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+  )
+}
+
+const Filter = ({ persons, setPersons, personsList, search, setSearch }) => {
+
+  const handleSearch = (event) => {
+    const value = event.target.value
+    setSearch(value)
+    personsList = persons.filter(person => 
+      person.name.toLowerCase().indexOf(value.toLowerCase()) !== -1)
+    setPersons(personsList)
+  }
+
+  return (
+    <p>filter shown with <input search={search} onChange={handleSearch} /></p>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
+
+  let personsList = persons.map((person, index) => {
+    let keyObj = {key: index, name: person.name, number: person.number}
+    return keyObj
+  })
+
+  return (
     <div>
       <h2>Phonebook</h2>
-      <p>filter shown with <input search={search} onChange={handleSearch} /></p>
-      <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          <p>name: <input value={newName} onChange={handleNameChange} /></p>
-          <p>number: <input value={newNumber} onChange={handleNumberChange} /></p>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {personsList.map(person => 
-        <Person key={person.key} name={person.name} number={person.number}/>
-      )}
+      <Filter persons={persons} setPersons={setPersons} personsList={personsList} search={search} setSearch={setSearch} />
+      <h3>add a new</h3>
+      <PersonForm 
+        persons={persons} setPersons={setPersons} 
+        newName={newName} setNewName={setNewName} 
+        newNumber={newNumber} setNewNumber={setNewNumber}
+      />
+      <h3>Numbers</h3>
+      <PersonList persons={persons} />
     </div>
   )
 }
