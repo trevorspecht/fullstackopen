8df64e63b49347fc1522ba1259a37843e758ebaf
trevorspecht <trevor.specht@gmail.com>
2021-11-11 22:58:19 -0500

    heroku prep

--- part3/index.js
+++ part3/index.js
@@ -1,8 +1,10 @@
 const { request, response } = require('express')
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 let notes = [  
     {    
@@ -70,7 +72,7 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
   
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
